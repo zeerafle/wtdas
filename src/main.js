46ba81b84a4cf4aa754446106b47ff7ca7a7a88e
Sam Fareez <vsefareez@gmail.com>
2024-07-31 23:56:41 +0800

    feat: add countdown

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import App from "./App.vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faShield, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import VueCountdown from '@chenfengyuan/vue-countdown';
 
 library.add(faShield, faChevronUp);
 const pinia = createPinia();
 const app = createApp(App);
 app.use(pinia);
 app.component("font-awesome-icon", FontAwesomeIcon);
+app.component(VueCountdown.name, VueCountdown);
 app.mount("#app");
